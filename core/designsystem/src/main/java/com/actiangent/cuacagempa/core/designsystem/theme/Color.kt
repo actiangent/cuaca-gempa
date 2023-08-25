aefbfa6b219444651919e73c764969a308a1bb06
actiangent <hisyam.fausta@gmail.com>
2023-08-25 10:13:08 +0700

    Update theme to custom Material 3 theme

--- core/designsystem/src/main/java/com/actiangent/cuacagempa/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/actiangent/cuacagempa/core/designsystem/theme/Color.kt
@@ -2,6 +2,40 @@ package com.actiangent.cuacagempa.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Note: Color naming is approximation.
+ */
+
+// Neutral Color
+internal val Jumbo10 = Color(0xFF1A1C1E) // Light On Background & On Surface, Dark Background & Surface
+internal val Jumbo90 = Color(0xFFE2E2E5) // Dark On Background and On Surface
+internal val Jumbo99 = Color(0xFFFCFCFF) // Light On Background and On Surface
+
+// Primary Color
+internal val Lochmara10 = Color(0xFFCEE5FF) // Light On Primary Container
+internal val Lochmara20 = Color(0xFF003353) // Dark On Primary
+internal val Lochmara30 = Color(0xFF004A75) // Dark Primary Container
+internal val Lochmara40 = Color(0xFF00639A) // Light Primary
+internal val Lochmara80 = Color(0xFF95CCFF) // Dark Primary
+internal val Lochmara90 = Color(0xFF004A75) // Light Primary Container
+internal val Lochmara100 = Color.White  // Light On Primary
+
+// Neutral Variant Color
+internal val PaleSky30 = Color(0xFF42474E) // Light On Surface Variant & Dark Surface Variant
+internal val PaleSky50 = Color(0xFF72777F) // Light Outline
+internal val PaleSky60 = Color(0xFF8C9198) // Dark Outline
+internal val PaleSky80 = Color(0xFFC2C7CF) // Dark On Surface Variant
+internal val PaleSky90 = Color(0xFFC2C7CF) // Light Surface Variant
+
+// Secondary Color
+internal val SlateGray10 = Color(0xFF0E1D2A) // Light On Secondary Container
+internal val SlateGray20 = Color(0xFF233240) // Dark On Secondary
+internal val SlateGray30 = Color(0xFF3A4857) // Dark Secondary Container
+internal val SlateGray40 = Color(0xFF51606F) // Light Secondary
+internal val SlateGray80 = Color(0xFFB9C8DA) // Dark Secondary
+internal val SlateGray90 = Color(0xFFD5E4F6) // Light Secondary Container
+internal val SlateGray100 = Color.White // Light On Secondary
+
 val StormCloud = Color(0xFF4E6969)
 val WeldonBlue = Color(0xFF7F9BA6)
 val VividSkyBlue = Color(0xFF00CCFF)
@@ -10,3 +44,6 @@ val Parchment = Color(0xFFFFFDB0)
 val ExuberantOrange = Color(0xFFEC622D)
 val Beluga = Color(0xFFF1F1F1)
 val Umbra = Color(0xFF1F1F1F)
+
+
+
--- core/designsystem/src/main/java/com/actiangent/cuacagempa/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/actiangent/cuacagempa/core/designsystem/theme/Theme.kt
@@ -1,25 +1,54 @@
 package com.actiangent.cuacagempa.core.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Umbra,
+private val DarkColorScheme = darkColorScheme(
+    primary = Lochmara80,
+    onPrimary = Lochmara20,
+    primaryContainer = Lochmara30,
+    onPrimaryContainer = Lochmara90,
+    secondary = SlateGray80,
+    onSecondary = SlateGray20,
+    secondaryContainer = SlateGray30,
+    onSecondaryContainer = SlateGray90,
+    background = Jumbo10,
+    onBackground = Jumbo90,
+    surface = Jumbo10,
+    onSurface = Jumbo90,
+    outline = PaleSky60,
+    surfaceVariant = PaleSky30,
+    onSurfaceVariant = PaleSky80
+
+    /* Other colors to override
+    error = ,
+    ...
+    */
 )
 
-private val LightColorPalette = lightColors(
-    primary = Beluga,
+private val LightColorScheme = lightColorScheme(
+    primary = Lochmara40,
+    onPrimary = Lochmara100,
+    primaryContainer = Lochmara90,
+    onPrimaryContainer = Lochmara10,
+    secondary = SlateGray40,
+    onSecondary = SlateGray100,
+    secondaryContainer = SlateGray90,
+    onSecondaryContainer = SlateGray10,
+    background = Jumbo99,
+    onBackground = Jumbo10,
+    surface = Jumbo99,
+    onSurface = Jumbo10,
+    outline = PaleSky50,
+    surfaceVariant = PaleSky90,
+    onSurfaceVariant = PaleSky30
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
+    /* Other colors to override
+    error = ,
+    ...
     */
 )
 
@@ -28,14 +57,14 @@ fun WeatherQuakeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
     } else {
-        LightColorPalette
+        LightColorScheme
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorScheme,
         content = content
     )
 }
